7da52f3e7b7419045535add8460599f923ecd676
plata4m <plata4m@gmail.com>
2020-04-12 17:43:12 -0300

    Update index.js

--- ServosNodeExpressJohnnyFive3/index.js
+++ ServosNodeExpressJohnnyFive3/index.js
@@ -4,7 +4,6 @@ var five = require("johnny-five"),
   board = new five.Board(),
   servos = null,
   servoPins = [9],
-  // servoPins = [9], // if you have only one servo
   express = require('express'),
   cors = require('cors'),
   app = express(),
@@ -21,42 +20,16 @@ board.on("ready", function() {
 
 var corsOptions = {
   origin: 'http://192.168.0.102',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200
 }
 app.use(cors(corsOptions));
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://192.168.0.102/servos/"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
 app.get('/servos/:mode/:value?', cors(), function (req, res) {
   if(servos) {
     var status = "OK";
-    var value = req.params.value; // optional, may be null
-    switch(req.params.mode) {
-      case "min": // 0 degrees
-        servos.min();
-        break;
-      case "max":
-        servos.max();
-        break;
-      case "stop": // use after sweep
-        servos.stop();
-        break;
-      case "sweep":
-        servos.sweep();
-        break;
-      case "to":
-        //if(value !== null)
-          console.log("Passou "+value);
-          servos.to( value );
-
-        break;
-      default:
-        status = "Unknown: " + req.params.mode;
-        break;
+    var value = req.params.value; 
+    if(value !== null)
+       console.log("Passou "+value);
+       servos.to( value );
      }
      console.log(status);
      res.send(status);
